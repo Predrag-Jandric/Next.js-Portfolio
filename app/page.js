7d65c2b4ae6424893869e72146f84ee603fe7d0d
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-13 00:33:38 +0200

    REDONE about section from old portfolio

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Skills from '../components/Skills'
+import About from '../components/About'
 
 export default function Home() {
   return (
     <main >
+      <About/>
       <Skills/>
     </main>
   )
