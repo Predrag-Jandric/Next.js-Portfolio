a50f036ef157a37ea710011dbbc72f3e365d5948
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-19 22:41:41 +0200

    refactured some code in HERO section by replacing imgs with nextJS's Image tag

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,33 +1,39 @@
+
 import React from 'react'
 import Image from 'next/image'
+
+import linkedInIcon from '../public/assets/all-social-icons/linkedIn.png'
+import gitHubIcon from '../public/assets/all-social-icons/github-hero.png'
+import youtube from '../public/assets/all-social-icons/youtube.png'
 
 function Hero() {
     return (
         <header className="hero">
-            <div className="hero__wrapper revealHeaderJS">
+            <section className="hero__wrapper revealHeaderJS">
                 <h1 className="hero__title">Hi, my name is Predrag</h1>
                 <p className="hero__p">Let's make the internet interesting! I am eager to fulfill my potential as <span className="hero__span">React Front-end developer</span> and create outstanding websites that deliver an exceptional user experience.</p>
-                <div className="hero__icons icons__global">
-                    <a href="https://www.linkedin.com/in/predrag-jandric/" target="_blank" className="hero__link">
-                        <img src="assets/img/all-social-icons/linkedIn.png" alt="image missing" className="hero__img" />
+                <article className="hero__icons icons__global">
+                    <a href="https://www.linkedin.com/in/predrag-jandric/" target="_blank" rel="noopener noreferrer" className="hero__link">
+                        <Image src={linkedInIcon} alt="image missing" className="hero__img" />
                     </a>
-                    <a href="https://github.com/Predrag-Jandric" target="_blank" className="hero__link">
-                        <img src="assets/img/all-social-icons/github-header.png" alt="image missing" className="hero__img" />
+                    <a href="https://github.com/Predrag-Jandric" target="_blank" rel="noopener noreferrer" className="hero__link">
+                        <Image src={gitHubIcon} alt="image missing" className="hero__img" />
                     </a>
-                    <a href="https://www.youtube.com/@predragjandric/videos" target="_blank"
-                        className="hero__link"><img src="assets/img/all-social-icons/youtube.png" alt="image missing" className="hero__img" /></a>
-                </div>
+                    <a href="https://www.youtube.com/@predragjandric/videos" target="_blank" rel="noopener noreferrer" className="hero__link">
+                        <Image src={youtube} alt="image missing" className="hero__img" />
+                    </a>
 
-                {/* planets */}
-                <img className="hero__earth mouseMoveParallaxJS"
-                    src="assets/img/header-imgs/earth.png" data-value="1" alt="image missing" />
-                <img className="hero__moon mouseMoveParallaxJS"
-                    src="assets/img/header-imgs/moon.png" data-value="2" alt="image missing" />
-                <img className="hero__star starParallaxJS" src="assets/img/header-imgs/star.png" alt="image missing" />
-            </div>
+                </article>
+
+                {/* place for new 3D planet */}
+
+            </section>
         </header>
 
     )
 }
 
 export default Hero
+
+
+// add particles as bg using "ts-particles npm package"
