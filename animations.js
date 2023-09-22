06090279742625728e63c0b010cdb14627e8dfb2
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-23 00:49:44 +0200

    ADDED all animations so far into single file and also DONE animations for PROJECTS section

---
+++ animations.js
@@ -0,0 +1,39 @@
+// THIS FILE CONTAINS ANIMATIONS FOR ALL SECTIONS.
+// MODIFY ANIMATIONS HERE AND NOWHERE ELSE.
+
+
+// animations for SKILLS section
+export const skillsAnimationVariants = {
+    initial: {
+        y: 70,
+        opacity: 0,
+    },
+    animate: (index) => {
+        return {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.2,
+                delay: 0.10 * index,
+            }
+        }
+    },
+}
+
+// animations for PROJECTS section
+export const projectsAnimationVariants = {
+    initial: {
+        y: 70,
+        opacity: 0,
+    },
+    animate: (index) => {
+        return {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.3,
+                delay: 0.3 * index,
+            }
+        }
+    },
+}
